4ef6c63a05cc2070d3bb94531251576d14d836fd
Maximiliano Cabrera <maxicabel07@gmail.com>
2022-09-09 16:54:10 -0300

    Agregando count a los detalles

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import './App.css';
 import './index.css' 
 import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer';
-import ItemCount from './components/ItemCount';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import Cart from './components/Cart';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
-
-  const onAdd = (quantity) => {
-    alert('Has añadido '+quantity+' unidades al carrito')
-
-  }
 
   return (
     <div>
@@ -27,12 +21,6 @@ function App() {
           <Route path="/item/:itemId" element={<ItemDetailContainer />} />
         </Routes>
     
-      <ItemCount
-        initial={1} 
-        stock={7}
-        onAdd={onAdd}
-      />
-      
     </BrowserRouter>
 
     </div>
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import ItemCount from '../components/ItemCount'
 
 const ItemDetail = ({data}) => {
 
-  return (
+    const [irAlCart, setIrAlCart] = useState(false);
+
+    const onAdd = (quantity) => {
+        setIrAlCart(true);
+    }
+    
+return (
         <div className="col-12 col-md-4">
             <h1>CAT: {data.categoria} </h1>
             <h2>Articulo: {data.nombre} </h2>
             <img src={data.img} alt=""/>
             <h3>${data.precio} </h3>
             <p>Detalle: {data.detalle} </p>
+            {
+                irAlCart 
+                ? <Link to='/cart'>Finalizar compra</Link>
+                : <ItemCount
+                initial={1} 
+                stock={7}
+                onAdd={onAdd}
+            />
+            }
         </div>
     )
 }
